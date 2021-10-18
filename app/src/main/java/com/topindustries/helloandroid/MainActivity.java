34defbea83064656e06f5ea994c1a3656eced4fb
Flavius Brandeu <flavius.brandeu@gmail.com>
2021-10-18 20:32:03 +0300

    feature: completed task1

--- app/src/main/java/com/topindustries/helloandroid/MainActivity.java
+++ app/src/main/java/com/topindustries/helloandroid/MainActivity.java
@@ -1,13 +1,22 @@
 package com.topindustries.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     EditText eName;
     Button bClick;
     TextView tName;
@@ -19,12 +28,38 @@ public class MainActivity extends AppCompatActivity {
         eName = (EditText) findViewById(R.id.eName);
         bClick = (Button) findViewById(R.id.bClick);
         tName = (TextView) findViewById(R.id.tName);
+        TypedArray ta = getResources().obtainTypedArray(R.array.buttonTextColors);
+        Set<String> buttonColorNames = new LinkedHashSet<String>();
+        for (int i = 0; i < ta.length(); i++) {
+            buttonColorNames.add(getResources().getResourceEntryName(ta.getResourceId(i, R.color.black)));
+        }
+        Spinner spinner = (Spinner) findViewById(R.id.colorsSpinner);
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, buttonColorNames.toArray());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
-    public void clicked(View view){
-        switch (view.getId()){
+    public void clicked(View view) {
+        switch (view.getId()) {
             case R.id.bClick:
-                tName.setText("Hello, " + eName.getText().toString());
+                String userName = eName.getText().toString();
+                tName.setText("Hello, " + userName);
+                GreetingsDialog greetingsDialog = new GreetingsDialog(userName);
+                greetingsDialog.show(getSupportFragmentManager(), "missiles");
         }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        String colorName = parent.getItemAtPosition(pos).toString();
+        int colorResourceId = getResources().getIdentifier(colorName, "color", this.getPackageName());
+        int color = getResources().getColor(colorResourceId);
+        bClick.setTextColor(color);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
 }
